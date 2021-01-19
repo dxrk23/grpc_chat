08a69a61951d4ed5043a2158022980beadc424d4
Dinmukhamed <d.yedil@astanait.edu.kz>
2021-01-19 12:09:17 +0600

    Добавьте файлы проекта.

---
+++ chat.Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using chat.Services;
+using ChatProject;
+using Grpc.Net.Client;
+
+namespace chat.Client
+{
+    class Program
+    {
+        public static CancellationToken CancellationToken { get; }
+        static async Task Main(string[] args)
+        {
+
+            Console.Write("Please enter your name: ");
+            var username = Console.ReadLine();
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new ChatService.ChatServiceClient(channel);
+            using (var chat = client.Join())
+            {
+                _ = Task.Run(async () =>
+                {
+                    while (await chat.ResponseStream.MoveNext(CancellationToken))
+                    {
+                        var response = chat.ResponseStream.Current;
+                        Console.WriteLine($"{response.User} : {response.Text}");
+                    }
+                });
+
+                await chat.RequestStream.WriteAsync(new Message { User = username, Text = $"{username} has joined the chat!" });
+
+                string line;
+
+                while ((line = Console.ReadLine()) != null)
+                {
+                    if (line.ToUpper() == "EXIT")
+                    {
+                        break;
+                    }
+                    await chat.RequestStream.WriteAsync(new Message { User = username, Text = line });
+                }
+
+                await chat.RequestStream.CompleteAsync();
+            }
+
+            Console.WriteLine("Disconnection started!");
+            await channel.ShutdownAsync();
+        }
+    }
+}
---
+++ chat/Services/GreeterService.cs
@@ -0,0 +1,34 @@
+using System.Threading.Tasks;
+using ChatProject;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace chat.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+
+        public override Task<HelloReply2> SayHello2(HelloRequest request, ServerCallContext context)
+        {
+            HelloReply2 reply = new HelloReply2();
+            reply.Replies.Add(new HelloReply { Message = "Hello " + request.Name + " 1" });
+            reply.Replies.Add(new HelloReply { Message = "Hello " + request.Name + " 2" });
+            reply.Replies.Add(new HelloReply { Message = "Hello " + request.Name + " 3" });
+
+            return Task.FromResult(reply);
+        }
+    }
+}
